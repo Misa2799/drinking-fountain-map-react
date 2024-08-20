75143c29b726306a62471ec23cd255fb518b2933
Misa Takeda <misatakeda.2799@gmail.com>
2024-08-20 09:59:12 -0700

    FEAT: display fountain information when a pin is clicked

--- frontend/src/components/get-fountains.tsx
+++ frontend/src/components/get-fountains.tsx
@@ -1,19 +1,12 @@
-import {
-  AdvancedMarker,
-  InfoWindow,
-  Pin,
-  useAdvancedMarkerRef,
-} from "@vis.gl/react-google-maps";
+import { AdvancedMarker, InfoWindow, Pin } from "@vis.gl/react-google-maps";
 import { LocationType } from "../types/location-type";
-import { useCallback, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 
 // pin fountains on map
 export const LocationMarkers = (props: { locations: LocationType[] }) => {
-  const [markerRef, marker] = useAdvancedMarkerRef();
-
   const [openMarkerId, setOpenMarkerId] = useState<string | null>(null);
 
-  const handleMakerClick = useCallback(
+  const handleMarkerClick = useCallback(
     (markerId: string) =>
       setOpenMarkerId((prevMarkerId) =>
         prevMarkerId === markerId ? null : markerId
@@ -25,29 +18,34 @@ export const LocationMarkers = (props: { locations: LocationType[] }) => {
 
   return (
     <>
-      {props.locations.map((location) => (
-        <AdvancedMarker
-          key={location.mapid}
-          ref={markerRef}
-          position={location.geo_point_2d}
-          onClick={() => {
-            handleMakerClick(location.mapid);
-          }}
-        >
-          <Pin
-            background={`#2E679A`}
-            glyphColor={"#C1C8CE"}
-            borderColor={"#C1C8CE"}
-          />
+      {props.locations.map((location, index) => {
+        const markerRef =
+          useRef<google.maps.marker.AdvancedMarkerElement | null>(null);
 
-          {openMarkerId === location.mapid && marker && (
-            <InfoWindow anchor={marker} onClose={handleClose}>
-              <h3>{location.name}</h3>
-              <h5>{location.location}</h5>
-            </InfoWindow>
-          )}
-        </AdvancedMarker>
-      ))}
+        return (
+          <AdvancedMarker
+            key={`${location.mapid}_${index}`}
+            ref={markerRef}
+            position={location.geo_point_2d}
+            onClick={() => {
+              handleMarkerClick(location.mapid);
+            }}
+          >
+            <Pin
+              background={`#2E679A`}
+              glyphColor={"#C1C8CE"}
+              borderColor={"#C1C8CE"}
+            />
+
+            {openMarkerId === location.mapid && markerRef.current && (
+              <InfoWindow anchor={markerRef.current} onClose={handleClose}>
+                <h3>{location.name}</h3>
+                <h5>{location.location}</h5>
+              </InfoWindow>
+            )}
+          </AdvancedMarker>
+        );
+      })}
     </>
   );
 };
